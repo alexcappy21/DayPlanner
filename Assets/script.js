019f15f04295ab8bf89a295a6981a47dcd182fd4
Alexandra Capodilupo <74632140+alexcappy21@users.noreply.github.com>
2021-06-01 22:41:17 -0400

    dayplanner

---
+++ Assets/script.js
@@ -0,0 +1,21 @@
+var currentDay = moment().format("dddd, MMMM Do YYY, h:mm:ss a");
+
+$("#currentDay").text(currentDay);
+
+#(document).ready(function () {
+
+    $("#hour-6 .description").val(localStorage.getItem("hour-6"));
+    $("#hour-7 .description").val(localStorage.getItem("hour-7"));
+    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+
+})
